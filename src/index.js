c29e6c4188616d7a82350e3fd539bccf38dd98cc
Ole Martin Gjersvik <olgj@equinor.com>
2020-10-29 14:49:54 +0100

    Backend first try.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,31 @@
-console.log("Hello world");
+const WebSocketServer = require('websocket').server;
+const http = require('http');
+ 
+var server = http.createServer(function(request, response) {
+    console.log((new Date()) + ' Received request for ' + request.url);
+    response.writeHead(404);
+    response.end();
+});
+server.listen(8080, function() {
+    console.log((new Date()) + ' Server is listening on port 8080');
+});
+ 
+wsServer = new WebSocketServer({
+    httpServer: server
+});
+
+let lastId = 0;
+let clients = [];
+
+wsServer.on('connect', function(ws) {
+    let id = lastId;
+    lastId += 1;
+    clients.push([id,ws]);
+    
+    ws.on('message', function(msg) {
+        clients.forEach(client => {
+            if(client[0] === id) continue;
+            client[1].sendUTF(msg.utf8Data);
+        });
+    });
+});
