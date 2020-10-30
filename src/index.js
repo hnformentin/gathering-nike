38da39c1001bbecb639276c043f6cf2793da55d8
Jørn Hegstad <JORHE@equinor.com>
2020-10-30 10:56:31 +0100

    Saving to blob storage

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const WebSocketServer = require('websocket').server;
 const serveStatic = require('serve-static');
+const { BlobServiceClient } = require('@azure/storage-blob');
+const { v1: uuid} = require('uuid');
+const blobName = 'message.txt';
 
 // Setting up the http server
 var app = express();
@@ -19,12 +22,33 @@ wsServer = new WebSocketServer({
 let lastId = 0;
 let clients = [];
 
+
+async function SaveBlob(message) {
+    console.log(message);
+    const AZURE_STORAGE_CONNECTION_STRING = process.env.AZURE_STORAGE_CONNECTION_STRING;
+    //TODO add correct connection string in fromConnectionString
+    const blobServiceClient = BlobServiceClient.fromConnectionString();
+    // Create a unique name for the container
+    const containerName = 'text';
+    // Get a reference to a container
+    const containerClient = blobServiceClient.getContainerClient(containerName);
+
+    // Get a block blob client
+    const blockBlobClient = containerClient.getBlockBlobClient(blobName);
+    // Upload data to the blob
+    if(message.length>0) {
+       const uploadBlobResponse = await blockBlobClient.upload(message, message.length);
+        console.log("Blob was uploaded successfully. requestId: ", uploadBlobResponse.requestId);
+    }
+}
+
 wsServer.on('connect', function(ws) {
     let id = lastId;
     lastId += 1;
     clients.push([id,ws]);
     
     ws.on('message', function(msg) {
+        SaveBlob(msg.utf8Data);
         clients.forEach(client => {
             if(client[0] === id) return;
             client[1].sendUTF(msg.utf8Data);
